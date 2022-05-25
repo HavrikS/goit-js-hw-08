ad25f8903dad35b88562d9b145da801af1a32c30
Sergey Havrik <s.gavrik83@gmail.com>
2022-05-25 22:47:44 +0300

    finished the third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const formData = {};
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('submit', formHandler);
+form.addEventListener('input', throttle(textFormormHandler, 500));
+
+function formHandler(e) {
+    e.preventDefault();
+    form.reset();
+    textarea.textContent = "";
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function textFormormHandler(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))    
+};
+
+populateTextarea();
+
+function populateTextarea() {
+    const sawedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (sawedMessage && Object.keys(sawedMessage).length !== 0 ) {
+        input.value = [sawedMessage.email];
+        textarea.textContent = [sawedMessage.message];
+    };
+};
 
