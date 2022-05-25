193134b84c5bd502208e994b33b053068cb80076
Sergey Havrik <s.gavrik83@gmail.com>
2022-05-25 16:02:49 +0300

    finished the second task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,6 @@ import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
 
 const galleryBox = document.querySelector('.gallery');
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = parseFloat(localStorage.getItem('videoplayer-current-time'));
+
+const onPlay = function ({seconds}) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    console.log(seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.setCurrentTime(currentTime);
+
 
